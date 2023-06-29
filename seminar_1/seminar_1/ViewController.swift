a57f94e21fe41ff1aa9d5ade5df282005ec6fc93
GodSamble <osgneb8359@naver.com>
2023-06-30 01:22:53 +0900

    [ADD] seminar_1 프로젝트 생성

---
+++ seminar_1/seminar_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_1
+//
+//  Created by 고영민 on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
