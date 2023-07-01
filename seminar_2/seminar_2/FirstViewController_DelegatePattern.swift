ec0823c237865632b70ef8a77c78f8c0a3086f1b
GodSamble <osgneb8359@naver.com>
2023-07-01 15:42:52 +0900

    [Add] #2 - ViewController 및 데이터 전달 구현

--- seminar_2/seminar_2/FirstViewController_DelegatePattern.swift
+++ seminar_2/seminar_2/FirstViewController_DelegatePattern.swift
@@ -1,1 +1,77 @@
+//
+//  ViewController.swift
+//  seminar_2
+//
+//  Created by 고영민 on 2023/07/01.
+//
 
+import UIKit
+
+final class FirstViewController_DelegatePattern: UIViewController {
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이름이 무엇인가요!?"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .blue
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다음으로!", for: .normal)
+        button.backgroundColor = .yellow
+        button.setTitleColor(.blue, for: .normal)
+        button.addTarget(self,
+                         action: #selector(pushToSecondViewController),
+                         for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setStyle()
+        setLayout()
+    }
+}
+
+private extension FirstViewController_DelegatePattern {
+    
+    func setStyle() {
+        
+        view.backgroundColor = .white
+    }
+    
+    func setLayout() {
+        
+        [nameLabel, nextButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                     nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)])
+        
+        
+        NSLayoutConstraint.activate([nextButton.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
+                                     nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+                                     nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+                                     nextButton.heightAnchor.constraint(equalToConstant: 48)])
+    }
+    
+    @objc
+    func pushToSecondViewController() {
+        
+        let secondViewController = SecondViewController_DelegatePattern()
+        secondViewController.delegate = self
+        self.navigationController?.pushViewController(secondViewController, animated: true)
+    }
+    
+}
+
+extension FirstViewController_DelegatePattern: DataBindProtocol {
+    func dataBind(text: String) {
+        nameLabel.text = text
+    }
+}
