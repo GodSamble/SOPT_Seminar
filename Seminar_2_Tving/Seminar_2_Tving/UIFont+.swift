3d6791783033b6bdbe03b6d1ae78da2e04ebe672
GodSamble <osgneb8359@naver.com>
2023-07-02 19:02:10 +0900

    [Add] #3 - SignIn View 구현

--- Seminar_2_Tving/Seminar_2_Tving/UIFont+.swift
+++ Seminar_2_Tving/Seminar_2_Tving/UIFont+.swift
@@ -8,20 +8,13 @@
 import UIKit
 
 extension UIFont {
-    
-    class func tvingSemiBold(ofSize size: CGFloat) -> UIFont{
-        return UIFont(name: "Pretendard-SemiBold", size: size)!
+    public enum pretendardType: String {
+        case medium = "Mediun"
+        case regular = "Regular"
+        case semibold = "SemiBold"
     }
-    
-    class func tvingBold(ofSize size: CGFloat) -> UIFont{
-        return UIFont(name: "Pretendard-Bold", size: size)!
-    }
-    
-    class func tvingMedium(ofSize size: CGFloat) -> UIFont{
-        return UIFont(name: "Pretendard-Medium", size: size)!
-    }
-    
-    class func tvingRegular(ofSize size: CGFloat) -> UIFont{
-        return UIFont(name: "Pretendard-Regular", size: size)!
+    static func pretendard(_ type: pretendardType, size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-\(type.rawValue)", size: size)!
     }
 }
+
---
+++ Seminar_2_Tving/Seminar_2_Tving/UIImage+.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+.swift
+//  Seminar_2_Tving
+//
+//  Created by 고영민 on 2023/07/02.
+//
+
+import UIKit
+
+public enum Image {
+    
+    public static let backButtonIcon = UIImage(named: "btn_before")
+    public static let passwordShowIcon = UIImage(named: "eye")!
+    public static let passwordHideIcon = UIImage(named: "eye-slash")!
+    public static let deleteIcon = UIImage(named: "x-circle")!
+    public static let tivingLogo = UIImage(named: "logo")!
+}
