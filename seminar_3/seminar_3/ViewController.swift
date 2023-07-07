00311058fe0e8b6b2eed56467c5a3675295ba9c5
GodSamble <osgneb8359@naver.com>
2023-07-07 16:52:35 +0900

    [Feat] #5 - 초기세팅

---
+++ seminar_3/seminar_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_3
+//
+//  Created by 고영민 on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
