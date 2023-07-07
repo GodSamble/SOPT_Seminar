01c71333fd6104aa3e04b7576cba2924c0701841
GodSamble <osgneb8359@naver.com>
2023-07-07 17:00:58 +0900

    [Add] #5 - 테이블뷰 구조체 구현

---
+++ seminar_3/seminar_3/Carrot.swift
@@ -0,0 +1,52 @@
+//
+//  Carrot.swift
+//  seminar_3
+//
+//  Created by 고영민 on 2023/07/07.
+//
+
+import UIKit
+
+struct Carrot {
+    let image : UIImage
+    let title : String
+    let place : String
+    let time : String
+    let tradeStatus :  Trade
+    let price : Int
+}
+
+
+@frozen
+enum Trade {
+    case reservation
+    case completed
+    case shared
+    case clear
+    
+    var title : String {
+        switch self {
+        case .reservation :
+            return "예약중"
+        case .completed :
+            return "거래완료"
+        case .shared :
+            return "나눔완료"
+        case .clear :
+            return ""
+        }
+    }
+    
+    var backgroundColor : UIColor {
+        switch self {
+        case .reservation :
+            return .systemGreen
+        case .completed :
+            return .black
+        case .shared :
+            return .gray
+        case .clear :
+            return .white
+        }
+    }
+}
