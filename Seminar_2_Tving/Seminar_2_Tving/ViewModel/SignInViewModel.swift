3be8ae4efb6f891854369ca7bbde53da74772ce4
GodSamble <osgneb8359@naver.com>
2023-10-21 22:16:27 +0900

    [Add] #7 - Rx 기반의 SignInViewModel 생성

--- Seminar_2_Tving/Seminar_2_Tving/ViewModel/SignInViewModel.swift
+++ Seminar_2_Tving/Seminar_2_Tving/ViewModel/SignInViewModel.swift
@@ -6,13 +6,26 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
 
 final class SignInViewModel {
+    
+    var id = BehaviorSubject<String>(value: "")
+    var password = BehaviorSubject<String>(value: "")
+    
+    var isValid: Observable<Bool> {
+        return Observable.combineLatest(id.asObserver(), password.asObservable())
+            .map { id, password in
+                return !id.isEmpty && !password.isEmpty
+            }
+    }
+    
+    
+    
     var nickname: String = "아무 입력 안됨"
     
-    func bindData(nickname: String) {
-        self.nickname = nickname
-    }
     
     func validateCredentials(id: String, password: String) -> Bool {
         return !id.isEmpty && !password.isEmpty
